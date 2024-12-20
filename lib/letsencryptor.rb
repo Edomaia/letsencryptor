dda65c81c03f71da8fa7964903dde26d736612c5
edoardo maraia <edoardo.maraia@gmail.com>
2024-12-20 08:34:24 +0100

    FIX: correct gem directory structure

---
+++ lib/letsencryptor.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "letsencryptor/version"
+require_relative "letsencryptor/aws_route53/client"
+module Letsencryptor
+  class Error < StandardError; end
+  # Your code goes here...
+
+  # class Configure
+  #   mattr_accessor :heroku_app_name, :heroku_token
+  # end
+
+  # TODO: add configuration to this gem!
+  # require heroku_client
+  # require aws_route_53_client
+end
---
+++ lib/letsencryptor/aws_route53/client.rb
@@ -0,0 +1,9 @@
+module Letsencryptor
+  module AwsRoute53
+    class Client
+      def test
+        puts "foo"
+      end
+    end
+  end
+end
